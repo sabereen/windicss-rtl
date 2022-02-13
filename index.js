8812a9af18f548bebeb1cd820c5911c1bd664d86
mojtaba ghasemzadeh <mqtstbh@gmail.com>
2022-02-13 15:06:44 +0330

    support rounded

--- index.js
+++ index.js
@@ -23,20 +23,27 @@ module.exports = plugin(({ addUtilities, addDynamic, theme }) => {
     },
   })
 
-  createDynamicLogical('ms', ['-webkit-margin-start', 'margin-inline-start'], theme('margin'))
-  createDynamicLogical('me', ['-webkit-margin-end', 'margin-inline-end'], theme('margin'))
-  createDynamicLogical('ps', ['-webkit-padding-start', 'padding-inline-start'], theme('padding'))
-  createDynamicLogical('pe', ['-webkit-padding-end', 'padding-inline-end'], theme('padding'))
+  createDynamicLogicalSpacing('ms', ['-webkit-margin-start', 'margin-inline-start'], theme('margin'))
+  createDynamicLogicalSpacing('me', ['-webkit-margin-end', 'margin-inline-end'], theme('margin'))
+  createDynamicLogicalSpacingWithoutNegative('ps', ['-webkit-padding-start', 'padding-inline-start'], theme('padding'))
+  createDynamicLogicalSpacingWithoutNegative('pe', ['-webkit-padding-end', 'padding-inline-end'], theme('padding'))
 
-  createDynamicLogical('start', ['inset-inline-start'], theme('inset'))
-  createDynamicLogical('end', ['inset-inline-end'], theme('inset'))
+  createDynamicLogicalSpacing('start', ['inset-inline-start'], theme('inset'))
+  createDynamicLogicalSpacing('end', ['inset-inline-end'], theme('inset'))
+
+  createDynamicRounded('rounded-s', ['border-start-start-radius', 'border-end-start-radius'])
+  createDynamicRounded('rounded-e', ['border-start-end-radius', 'border-end-end-radius'])
+  createDynamicRounded('rounded-te', 'border-start-end-radius')
+  createDynamicRounded('rounded-be', 'border-end-end-radius')
+  createDynamicRounded('rounded-ts', 'border-start-start-radius')
+  createDynamicRounded('rounded-bs', 'border-end-start-radius')
 
   /**
    * @param {string} className 
    * @param {string | string[]} prop 
    * @param {any} themeValues 
    */
-  function createDynamicLogical(className, prop, themeValues) {
+  function createDynamicLogicalSpacing(className, prop, themeValues) {
     addDynamic(className, ({ Utility }) => {
       return Utility.handler
         .handleStatic(themeValues)
@@ -56,6 +63,69 @@ module.exports = plugin(({ addUtilities, addDynamic, theme }) => {
         '{value}',
         '[value]',
       ].map(k => k[0] === '-' ? `-${className}-${k.substring(1)}` : `${className}-${k}`),
+    })
+  }
+
+  /**
+   * @param {string} className 
+   * @param {string | string[]} prop 
+   * @param {any} themeValues 
+   */
+  function createDynamicLogicalSpacingWithoutNegative(className, prop, themeValues) {
+    addDynamic(className, ({ Utility }) => {
+      return Utility.handler
+        .handleStatic(themeValues)
+        .handleSquareBrackets()
+        .handleSpacing()
+        .handleFraction()
+        .handleSize()
+        .handleVariable()
+        .createProperty(prop)
+    }, {
+      group: 'logical',
+      completions: [
+        ...Object.keys(themeValues),
+        // eslint-disable-next-line no-template-curly-in-string
+        '${var}',
+        '{value}',
+        '[value]',
+      ].map(k => k[0] === '-' ? `-${className}-${k.substring(1)}` : `${className}-${k}`),
+    })
+  }
+
+  /**
+   * @param {string} className 
+   * @param {string | string[]} prop 
+   */
+  function createDynamicRounded(className, prop) {
+    const themeValues = theme('borderRadius')
+    const defaultRoundeds = ['rounded-s', 'rounded-e', 'rounded-ts', 'rounded-te', 'rounded-bs', 'rounded-be']
+
+    addDynamic(className, ({ Utility }) => {
+      const raw = defaultRoundeds.includes(Utility.raw) ? Utility.raw + '-DEFAULT' : Utility.raw
+
+      return Utility.clone(raw).handler
+        .handleStatic(themeValues)
+        .handleSquareBrackets()
+        .handleFraction()
+        .handleNxl(n => (n * 0.5) + 'rem')
+        .handleSize()
+        .handleVariable()
+        .createProperty(prop)
+    }, {
+      group: 'logical',
+      completions: [
+        ...Object.keys(themeValues),
+        // eslint-disable-next-line no-template-curly-in-string
+        '${var}',
+        '{value}',
+        '[value]',
+        'Nxl',
+      ].map(k =>
+        k === 'DEFAULT' ? `${className}` :
+        k[0] === '-' ? `-${className}-${k.substring(1)}` :
+        `${className}-${k}`
+      ),
     })
   }
 })
